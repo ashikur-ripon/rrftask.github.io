fde4f50914bd3ce7ccc85511eac66aec0ff77f07
Ashikur Rahman Ripon <ashikur.amath@gmail.com>
2017-06-17 16:52:01 +0600

    class9

---
+++ psd2html/class9/assets/js/main.js
@@ -0,0 +1,86 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+		
+		//owl carousel 		
+		$(".case-studies-carousel").owlCarousel({
+			items: 3,
+			margin: 30,
+			loop: true,
+			dots: true,
+			nav: false,
+		});
+
+		
+		$(".testimonial-carousel").owlCarousel({
+			items: 1,
+			loop: true,
+			dots: true,
+			nav: false,
+		});  
+		//end owl carousel
+
+		//company carousel 
+		$(".logo-carousel").owlCarousel({
+			items: 6,
+			loop: true,
+			dots: false,
+			nav: false,
+		});  
+		// end company carousel
+
+		
+		
+		// home2 js
+
+		$(".theme2-case-studies-carousel").owlCarousel({
+			items: 3,
+			margin: 30,
+			loop: true,
+			dots: true,
+			nav: false,
+		});
+		
+		$(".theme2-single-testimonial-carousel").owlCarousel({
+			items: 1,
+			loop: true,
+			dots: true,
+			nav: false,
+		});  
+		
+		$(".theme2-blog-box-carousel").owlCarousel({
+			items: 3,
+			margin: 30,
+			loop: true,
+			dots: true,
+			nav: false,
+		});
+		
+		
+		// home3 js
+		$(".homepage-slides").owlCarousel({
+			items: 1,
+			loop: true,
+			dots: true,
+			nav: true,
+			navText: ["<i class='zmdi zmdi-arrow-left'>",
+					  "<i class='zmdi zmdi-arrow-right'>"]
+		});
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
