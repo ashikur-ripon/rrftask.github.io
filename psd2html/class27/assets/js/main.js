2046c744c288589e761efabd72e7138223e9b7d6
Ashikur Rahman Ripon <ashikur.amath@gmail.com>
2017-08-01 23:24:43 +0600

    class27

---
+++ psd2html/class27/assets/js/main.js
@@ -0,0 +1,176 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+		
+		//owl carousel 		
+		$(".case-studies-carousel").owlCarousel({
+		
+			margin: 30,
+			loop: true,
+			dots: true,
+			nav: false,
+			responsive : {
+				0 : {
+					items: 1
+				},
+				768 : {
+					items: 2
+				},
+				992 : {
+					items: 3
+				}				
+			}
+		});
+		
+
+		
+		
+		$(".testimonial-carousel").owlCarousel({
+			items: 1,
+			loop: true,
+			dots: true,
+			nav: false,
+		});  
+		//end owl carousel
+
+		//company carousel 
+		$(".logo-carousel").owlCarousel({
+		
+			loop: true,
+			dots: false,
+			nav: false,
+			responsive : {
+				0 : {
+					items: 2
+				},
+				768 : {
+					items: 4
+				},
+				992 : {
+					items: 6
+				}				
+			}
+		});  
+		// end company carousel
+
+		
+		
+		// home2 js
+
+		$(".theme2-case-studies-carousel").owlCarousel({
+			items: 3,
+			margin: 30,
+			loop: true,
+			dots: true,
+			nav: false,		
+		});
+		
+		$(".theme2-single-testimonial-carousel").owlCarousel({
+			items: 1,
+			loop: true,
+			dots: true,
+			nav: false,
+		});  
+		
+		$(".theme2-blog-box-carousel").owlCarousel({
+			items: 3,
+			margin: 30,
+			loop: true,
+			dots: true,
+			nav: false
+		});
+		
+		
+		// home3 js
+		$(".homepage-slides").owlCarousel({
+			items: 1,
+			loop: true,
+			dots: true,
+			nav: true,
+			navText: ["<i class='fa fa-arrow-left'>",
+					  "<i class='fa fa-arrow-right'>"]
+		});
+		
+		
+		// home4 js
+		
+		// off canvas menu
+		$(".menu-trigger").on('click',function(){
+			$(".off-canvas-menu").addClass("show-off-canvas-menu");
+			$(".off-canvas-menu-shade").addClass("active");
+		});
+		$(".menu-close,.off-canvas-menu-shade").on('click',function(){
+			$(".off-canvas-menu").removeClass("show-off-canvas-menu");
+			$(".off-canvas-menu-shade").removeClass("active");
+		});
+		
+		
+		$(".search-trigger").on('click',function(){
+			$(".search-bar-wrap").addClass("active");
+		});
+		$(".search-close").on('click',function(){
+			$(".search-bar-wrap").removeClass("active");
+		});
+		
+		
+		
+		$(".boxed-layout").on('click',function(){
+			$("body").addClass("boxed-layout").removeClass("wide-layout");
+			$(".layout-changer span").removeClass("active");
+			$(this).addClass("active");
+		});
+		
+		$(".wide-layout").on('click',function(){
+			$("body").addClass("wide-layout").removeClass("boxed-layout");
+			$(".layout-changer span").removeClass("active");
+			$(this).addClass("active");
+		});
+		
+		
+		
+		
+		
+		
+		
+		
+		$(".single-testimonial-box").hover(function() {
+			$(".single-testimonial-box").removeClass('active');
+			$(this).addClass('active');
+		});
+		
+		$(".video-play-btn").magnificPopup ({
+			type: 'video'
+		});
+		
+		
+		// disable map scrolling
+		$('.map-area-wrapper')
+			.click(function(){
+					$(this).find('iframe').addClass('clicked')})
+			.mouseleave(function(){
+					$(this).find('iframe').removeClass('clicked')});
+					
+					
+		// slicknav
+		$('#traffic-menu').slicknav({
+			prependTo: '#mobile-menu-wrap',
+			allowParentLinks: true 
+		});
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
