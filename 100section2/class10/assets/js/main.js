97719cc246a58d254d269b41510433be02b48459
Ashikur Rahman Ripon <ashikur.amath@gmail.com>
2017-08-02 22:33:02 +0600

    class10

---
+++ 100section2/class10/assets/js/main.js
@@ -0,0 +1,32 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+		$(".homepage-slides").owlCarousel({
+			items: 1,
+			loop: true,
+			autoplay: true,
+			nav: true,
+			navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"]
+		});
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
