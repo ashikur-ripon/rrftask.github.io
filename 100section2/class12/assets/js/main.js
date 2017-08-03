0713da4c8a8fa767b0ec7ef5bf9f34d6548f98a2
Ashikur Rahman Ripon <ashikur.amath@gmail.com>
2017-08-04 00:53:57 +0600

    class12

---
+++ 100section2/class12/assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+		        "use strict";
+$(".js-modal-btn").modalVideo();
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
