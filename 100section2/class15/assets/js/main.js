7ac3244444385f5eafe19af7a6386d5da82515d8
Ashikur Rahman Ripon <ashikur.amath@gmail.com>
2017-08-05 12:48:12 +0600

    class15

---
+++ 100section2/class15/assets/js/main.js
@@ -0,0 +1,36 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+		$(".portfolio-filter li").on('click', function() {
+			
+			var filterData = $(this).attr("data-filter");
+			
+			$(".portfolio-list").isotope({
+				filter: filterData
+			});
+			
+			$(".portfolio-filter li").removeClass('active');
+			$(this).addClass('active');
+		});
+		
+
+		$(".portfolio-list").isotope({
+			itemSelector: '.single-portfolio-item',
+			percentPosition: true,
+			masonry: {
+				columnWidth: '.single-portfolio-item',
+				horizontalOrder: true 
+			}
+		});
+    });
+
+
+    jQuery(window).load(function(){
+        
+    });
+
+
+}(jQuery));	
