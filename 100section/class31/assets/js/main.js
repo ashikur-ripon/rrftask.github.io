4d4b30705fe166d59e11ead0fd9df9c306038382
Ashikur Rahman Ripon <ashikur.amath@gmail.com>
2017-09-21 01:09:16 +0600

    class31

---
+++ 100section/class31/assets/js/main.js
@@ -0,0 +1,22 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+var s = skrollr.init();
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
