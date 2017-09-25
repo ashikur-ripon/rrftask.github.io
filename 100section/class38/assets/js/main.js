504e9cffb01e47b1ffa8d4260101aa5eaee691d5
Ashikur Rahman Ripon <ashikur.amath@gmail.com>
2017-09-25 18:50:31 +0600

    class38

--- 100section/class38/assets/js/main.js
+++ 100section/class38/assets/js/main.js
@@ -1,40 +1,28 @@
-(function($) {
-    'use strict';
-    
-    jQuery(document).ready(function() {
-        
-    		$(".homepage-slides").owlCarousel({
-        	items: 1,
-        	autoplay: true,
-        	loop:true,
-        	dots: true,
-        	smartSpeed: 500,
-        	nav: true,
-        	navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-        	mouseDrag: false,
-        	touchDrag: false,
-        	smartSpeed:800,
-        	autoplayTime: 1000
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".homepage-slides").owlCarousel({
+            items: 1,
+            loop: true,
+            dots: true,
+            nav: true,
+            lopps: true,
+            autoplay: false,
+            navText: ["<i class='fa fa-arrow-left'></i>","<i class='fa fa-arrow-right'></i>"]
         });
-
-
-
-        $(".homepage-slides").on("translate.owl.carousel", function() {
-			$(".slide-text").removeClass("animated fadeIn").css("opacity", "0");
-			$(".single-slide-item h1").removeClass("animated fadeInLeft").css({"-webkit-animation-delay":".5s", "animation-delay":".5s", "opacity":"0"});
-			$(".single-slide-item p").removeClass("animated fadeInLeft").css({"-webkit-animation-delay":".8s", "animation-delay":".8s", "opacity": "0"});
-			$(".single-slide-item .boxed-btn").removeClass("animated fadeInLeft").css({"-webkit-animation-delay":"1.2s","animation-delay":".1.2s", "opacity":"0"});
-            
-		});
-
-		$(".homepage-slides").on("translated.owl.carousel", function() {
-			$(".slide-text").addClass("animated fadeIn").css("opacity", "1");
-			$(".single-slide-item h1").addClass("animated fadeInLeft").css({"-webkit-animation-delay":".5s", "animation-delay":".5s", "opacity":"1"});
-			$(".single-slide-item p").addClass("animated fadeInLeft").css({"-webkit-animation-delay": ".8s", "animation-delay":".8s", "opacity": "1"});
-			$(".single-slide-item .boxed-btn").addClass("animated fadeInLeft").css({"-webkit-animation-delay":"1.2s", "animation-delay":".1.2s", "opacity":"1"});
-           
-		});
         
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
         
-    });// End of jQuery document function   
-}(jQuery));
+    });
+
+
+}(jQuery));	
