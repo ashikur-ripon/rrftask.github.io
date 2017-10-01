03c26be432a631e610d04176669c5ed4eecbbeef
Ashikur Rahman Ripon <ashikur.amath@gmail.com>
2017-10-01 16:08:38 +0600

    class44

--- 100section/class44/assets/js/main.js
+++ 100section/class44/assets/js/main.js
@@ -4,7 +4,14 @@
     jQuery(document).ready(function($){
 
 
-       
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+
+
+        
 
 
     });
